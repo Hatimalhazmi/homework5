8ab42f8db0696abd48059e1711d0e4ace5da6d2e
Hatimalhazmi <60146633+Hatimalhazmi@users.noreply.github.com>
2021-01-19 21:19:37 -0500

    Update test_array.py

--- tests/test_array.py
+++ tests/test_array.py
@@ -3,8 +3,9 @@ import pytest
 from mypkg.my_answers import generateParenthesis
 
 
-
-def generateParenthesis(N):
-    assert unit.generateParenthesis('param') == 'expect', 'Case 1'
-    assert unit.generateParenthesis('param') == 'expect', 'Case 2'
-
+def generateParenthesisCase(N):
+        func = Solution().generateParenthesis
+        self.assertEqual(func(0), [""])
+        self.assertEqual(func(-1), [])
+        self.assertEqual(func(2), ["(())","()()"])
+        self.assertEqual(func(3), ["((()))","(()())","(())()","()(())","()()()"])
