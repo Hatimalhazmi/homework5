9c456b56a822b3c164299a39c3442484fd889bab
Hatimalhazmi <60146633+Hatimalhazmi@users.noreply.github.com>
2021-01-19 21:43:59 -0500

    Update test_array.py

--- tests/test_array.py
+++ tests/test_array.py
@@ -4,8 +4,5 @@ from mypkg.my_answers import generateParenthesis
 
 
 def generateParenthesisCase(N):
-        func = Solution().generateParenthesis
-        self.assertEqual(func(0), [""])
-        self.assertEqual(func(-1), [])
-        self.assertEqual(func(2), ["(())","()()"])
-        self.assertEqual(func(3), ["((()))","(()())","(())()","()(())","()()()"])
+    assert testCase.generateParenthesis(3) == ["((()))", "(()())", "(())()", "()(())", "()()()"]
+
