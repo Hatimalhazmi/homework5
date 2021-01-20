8d88afcaa2fd99a14c87fbb5dd9d285606769827
Hatimalhazmi <60146633+Hatimalhazmi@users.noreply.github.com>
2021-01-20 09:10:03 -0500

    Update my_answers.py

--- src/mypkg/my_answers.py
+++ src/mypkg/my_answers.py
@@ -2,80 +2,64 @@
 #!/usr/bin/python
 
 """
-Python Functions and Recursions
-
+Object Oriented Programming and Python Classes
 """
+
 """
 QUESTION 1: 
 ========================================================================================================
-Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
-For example, given n = 3, a solution set is:
-[
-  "((()))",
-  "(()())",
-  "(())()",
-  "()(())",
-  "()()()"
-]
-Write a function named generateParenthesis that takes an integer as an input and returns a list of strings 
-as an output. Note that you can define a function inside a function if necessary.
-def generateParenthesis(N):
-    results = []
+Write a class with name WindowAverage which is initialized by a window size and returns an average of
+a stream of integers over a window by calling a method next. 
+Example:
+===========================
+    w = WindowAverage(2)
+    w.next(2) = 2
+    w.next(4) = (2+4)/2
+    w.next(3) = (4+3)/2
+    w.next(2) = (2+3)/2
+    
+Hints: You should have __init__ and next methods and can use a data structure - double-ended queue
+from collections module.    
 """
-import itertools
-
-def generateParenthesis(N):
-    results = []
-
-    def backtrack(parenthesis, opening, closing):
-        if len(parenthesis) == 2 * N:
-            results.append(parenthesis)
-            return
-
-        if opening < N:
-            backtrack(parenthesis + '(', opening + 1, closing) # generate opening bracket
-
-        if closing < opening:
-            backtrack(parenthesis + ')', opening, closing + 1) # generate closing bracket
-
-    backtrack('', 0, 0)
-    return results
-
 
 """
 QUESTION 2: 
 ========================================================================================================
-Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
-Note: For the purpose of this problem, we define empty string as valid palindrome.
-Example 1:
-========================================
-Input: "A man, a plan, a canal: Panama"
-Output: true
-Example 2:
-=========================================
-Input: "race a car"
-Output: false
-Write a function named isPalindrome that takes a string as an input and returns a bool as an output.
+Write a class with name AlternateIterator which is initialized by two lists v1 and v2
+and returns the elements alternatively every time a method next is called and stops
+iteration when both lists are exhausted
+Example:
+===========================
+    Input: l1 = [1, 2, 3]
+           l2 = [4, 5, 6, 7]
+        
+    Output: [1, 4, 2, 5, 3, 6, 7]
+ 
+    Usage:
+    iter = AlternateIterator(l1, l2)
+    res = []
+    while iter.has_next():
+        res.append(iter.next())
+Hints: You should have three methods: __init__, next, and has_next. 
+"""
 
 """
-class Solution {
-    public static boolean isPalindrome(String s) {
-        if (s == null) return false;
-        if (s.length() == 0) return true;
-        s = s.toLowerCase().trim();
-        int i = 0;
-        int j = s.length() - 1;
-        while (i < j) {
-            while (i < j && !Character.isLetterOrDigit(s.charAt(i))) i++ ;
-            while (i < j && !Character.isLetterOrDigit(s.charAt(j))) j--;
-            if (s.charAt(i++) != s.charAt(j--)) return false;
-        }
-        return true;
-    }
-    public static void main(String[] args) {
-        isPalindrome("A man, a plan, a canal: Panama");
-    }
-
-
+QUESTION 3:
+========================================================================================================
+Write a class ModifiedIterator that inherits the class AlternateIterator you wrote in Q2 that overwrites
+the method next so that it return twice of the value from l2.
+Example:
+===========================
+    Input: l1 = [1, 2, 3]
+           l2 = [4, 5, 6, 7]
+        
+    Output: [1, 8, 2, 10, 3, 12, 14]
+    
+    Usage:
+    iter = ModifiedIterator(l1, l2)
+    res = []
+    while iter.has_next():
+        res.append(iter.next())
+"""
 
 
