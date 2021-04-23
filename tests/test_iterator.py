d5edd0cc907341842dd33c7457774f4a357351ed
Hatimalhazmi <60146633+Hatimalhazmi@users.noreply.github.com>
2021-04-23 07:44:16 -0400

    Update test_iterator.py

--- tests/test_iterator.py
+++ tests/test_iterator.py
@@ -5,6 +5,5 @@ from mypkg.my_answers import iterator
 #class Test(unittest.TestCase):
 
     def test_iterator(self):
-       self.assertEqual(iterator(10), 0 1 2 3 4 5 6 7 8 9 10 )
+        assert iterator(10) ==  0 1 2 3 4 5 6 7 8 9 10
         
-if __name__ == "__main__":
