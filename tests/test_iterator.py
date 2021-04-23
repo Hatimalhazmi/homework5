081d94047f107d1526d710b20bb1e7bc03133649
Hatimalhazmi <60146633+Hatimalhazmi@users.noreply.github.com>
2021-04-23 08:30:25 -0400

    Update test_iterator.py

--- tests/test_iterator.py
+++ tests/test_iterator.py
@@ -4,4 +4,7 @@ from mypkg.my_answers import iterator
 
 #class Test(unittest.TestCase):
 def test_iterator():
-    assert iterator(10) == 1
+ 
+result = iterator(10)
+	expected = 1 2 3 4 5 6 7 8 9
+	assert result == expected
