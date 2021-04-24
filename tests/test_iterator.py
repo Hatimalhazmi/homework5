a2efcf697902dd57b42d6952ddb3670235130450
Hatimalhazmi <60146633+Hatimalhazmi@users.noreply.github.com>
2021-04-24 07:27:30 -0400

    Update test_iterator.py

--- tests/test_iterator.py
+++ tests/test_iterator.py
@@ -5,7 +5,7 @@ from mypkg.my_answers import iterator
 #class Test(unittest.TestCase):
 def test_iterator():
  	#for i in iterator(1):
-    	assert(iterator(1) == iterator(1))
+    	assert(iterator(1) = iterator(1))
 
 		#ans = Solution()
         	#self.assertEqual(ans.i == 1 )
