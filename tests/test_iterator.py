3ea1a059ae88db5d1782b01ca9cd2afb6f57c906
Hatimalhazmi <60146633+Hatimalhazmi@users.noreply.github.com>
2021-04-24 09:19:58 -0400

    Update test_iterator.py

--- tests/test_iterator.py
+++ tests/test_iterator.py
@@ -1,13 +1,10 @@
 import pytest
 from mypkg.my_answers import iterator
-def test_always_passes():
-    assert True
 
 
-#class Test(unittest.TestCase):
 def test_iterator():
  	for i in iterator(5):
-    		assert i == 5
+    		assert ((i,end = ", ") == 1, 2, 3, 4, 5,)
 
 		#ans = Solution()
         	#self.assertEqual(ans.i == 1 )
