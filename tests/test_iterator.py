36b8199bd0daf9edcb5ccb6ec5e7b6a70462a35c
Hatimalhazmi <60146633+Hatimalhazmi@users.noreply.github.com>
2021-04-24 08:26:04 -0400

    Update test_iterator.py

--- tests/test_iterator.py
+++ tests/test_iterator.py
@@ -1,15 +1,19 @@
 import pytest
 from mypkg.my_answers import iterator
+def test_always_passes():
+    assert True
 
+def test_always_fails():
+    assert False
 
 #class Test(unittest.TestCase):
-def test_iterator():
+#def test_iterator():
  	#for i in iterator(1):
-    	(assert iterator(1) == 1)
+    	#(assert iterator(1) == 1)
 
 		#ans = Solution()
         	#self.assertEqual(ans.i == 1 )
 	
-if __name__ == "__main__":
+#if __name__ == "__main__":
 
-    pytest.main()
+   # pytest.main()
