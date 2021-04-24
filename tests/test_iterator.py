57b3c9728c181ac012a296746e8a2cf605a172ae
Hatimalhazmi <60146633+Hatimalhazmi@users.noreply.github.com>
2021-04-24 10:34:32 -0400

    Update test_iterator.py

--- tests/test_iterator.py
+++ tests/test_iterator.py
@@ -5,10 +5,10 @@ from mypkg.my_answers import iterator
 def test_iterator():
  	for i in iterator(5):
 		
-		assert i[0] == 1
-	    	assert i[1] == 2
-   		assert i[2] == 3
-    		assert i[3] == 4
+		assert i[1] == 1
+	    	assert i[2] == 2
+   		assert i[3] == 3
+    		assert i[4] == 4
 			 
 		#ans = Solution()
         	#self.assertEqual(ans.i == 1 )
