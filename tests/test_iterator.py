cd5393e400e4221d21be54ddf92979ed7e8d48bb
Hatimalhazmi <60146633+Hatimalhazmi@users.noreply.github.com>
2021-04-24 21:05:35 -0400

    Update test_iterator.py

--- tests/test_iterator.py
+++ tests/test_iterator.py
@@ -5,6 +5,5 @@ from mypkg.my_answers import iterator
 
 
 def test_iterator():
-  for i in iterator(2):
-    		assert (i) == 1
-	      		   == 2
+  i = [i for i in iterator(5)]
+    		assert (i) == [1, 2, 3, 4, 5]
